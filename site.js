c6d46fb4c3c66f174f342220e6b1703feb20b62c
bri.depinto <briannadepinto@gmail.com>
2024-12-12 14:36:17 -0500

    added the date

--- site.js
+++ site.js
@@ -42,6 +42,50 @@ const vue_app = Vue.createApp({
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            getMonthText(dateArray) {
+
+                  let year = dateArray[0];
+                  let day = dateArray[2];
+                  switch (dateArray[1]) {
+                        case 1:
+                              month = "January";
+                              break;
+                        case 2:
+                              month = "February";
+                              break;
+                        case 3:
+                              month = "March";
+                              break;
+                        case 4:
+                              month = "April";
+                              break;
+                        case 5:
+                              month = "May";
+                              break;
+                        case 6:
+                              month = "June";
+                              break;
+                        case 7:
+                              month = "July";
+                              break;
+                        case 8:
+                              month = "August";
+                              break;
+                        case 9:
+                              month = "September";
+                              break;
+                        case 10:
+                              month = "October";
+                              break;
+                        case 11:
+                              month = "November";
+                              break;
+                        case 12:
+                              month = "December";
+                              break;
+                  }
+                  return `${month} ${day}, ${year}`
+            }
       }
 })
 
